5ab436be804afb662cc0ef7368fdf087f23221f0
yaswanth30-04 <116426380+yaswanth30-04@users.noreply.github.com>
2023-12-16 11:16:35 +0530

    initital

--- components/HomePage/Ceoimage.tsx
+++ components/HomePage/Ceoimage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ceoimage from "@public/ceo.jpg"
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
+import { selectallproducts } from '@app/redux/feautres/products/product-slice'
 const Ceoimage = () => {
+  const products = useSelector(selectallproducts);
+  if (!products || !products.length) {
+    return <></>
+  }
   return (
     <div className='relative w-full h-[400px] rounded-md'>
 
